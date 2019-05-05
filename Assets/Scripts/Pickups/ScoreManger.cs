e8654f1a076dead59c6f3c3f126968818cc7b8e3
Moni777 <45233817+Moni777@users.noreply.github.com>
2019-05-05 14:36:55 +0200

    add score

---
+++ Assets/Scripts/Pickups/ScoreManger.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManger : MonoBehaviour
+{
+    public Text redText;
+    public Text blueText;
+
+    public float scoreCountRed;
+    public float scoreCountBlue;
+    public float pointsPerSecond;
+
+    public bool scoreIncreasingRed;
+    public bool scoreIncreasingBlue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (scoreIncreasingRed)
+        {
+            scoreCountRed += pointsPerSecond * Time.deltaTime;
+        }
+        if (scoreIncreasingBlue)
+        {
+            scoreCountBlue += pointsPerSecond * Time.deltaTime;
+        }
+        redText.text = "Red team  " + Mathf.Round (scoreCountRed);
+        blueText.text = Mathf.Round(scoreCountBlue) + "  Blue team";
+    }
+}
