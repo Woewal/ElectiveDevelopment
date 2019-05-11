3d2828fcc32a589af493ff6e08f9132473c47f75
Moni777 <45233817+Moni777@users.noreply.github.com>
2019-05-12 00:22:32 +0200

    health bar

---
+++ Assets/Scripts/Player/Health.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+  public Slider Slider;
+    public float CurrentHP;
+    private float MaxHP = 100;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CurrentHP = MaxHP;
+        
+    }
+
+    private void takeDamage(float damage)
+    {
+        CurrentHP -= damage;
+        ShowHPSlider();
+        if (CurrentHP <= 0)
+        {
+            CurrentHP = 0;
+
+            CurrentHP = 100;
+
+        }
+    }
+
+    public void ShowHPSlider()
+    {
+        Slider.value = CurrentHP / (float)MaxHP;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
