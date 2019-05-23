b380688647594f901b6c5dbdcab8bf9cac544d0b
Dennis Woering <d.woering@hotmail.com>
2019-05-23 10:40:54 +0200

    Added pickup types + added random generated level

---
+++ Assets/Scripts/Level/LevelGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//D
+public class LevelGenerator : MonoBehaviour
+{
+	[SerializeField] GameObject[] _walls;
+	[SerializeField] float _spawnChance;
+
+
+	void Start()
+	{
+		foreach(var wall in _walls)
+		{
+			var randomValue = Random.value;
+
+			if (randomValue < _spawnChance)
+				wall.SetActive(false);
+		}
+	}
+
+}
